e08eb83576877e3f7e5e0ea658840cdfabeea395
abubaker <mohammad_abubaker@yahoo.com>
2021-01-31 22:38:40 +0500

    Implemented code to preserve data on configuration change using onSaveInstancceState (outState: Bundle)

--- app/src/main/java/org/abubaker/timefighter/MainActivity.kt
+++ app/src/main/java/org/abubaker/timefighter/MainActivity.kt
@@ -2,12 +2,16 @@ package org.abubaker.timefighter
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // DEBUGGING - Setup
+    private val TAG = MainActivity::class.java.simpleName
 
     // We will connect views to these variables later on
     private lateinit var gameScoreTextView: TextView
@@ -29,6 +33,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // DEBUGGING - Trace changes here
+        Log.d(TAG, "onCreate called. Score is: $score")
+
+
         gameScoreTextView = findViewById(R.id.game_score_text_view)
         timeLeftTextView = findViewById(R.id.time_left_text_view)
         tapButton = findViewById(R.id.tap_me_button)
@@ -37,7 +45,15 @@ class MainActivity : AppCompatActivity() {
             incrementScore()
         }
 
-        resetGame()
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeft = savedInstanceState.getInt(TIME_LEFT_KEY)
+
+            restoreGame()
+
+        } else {
+            resetGame()
+        }
     }
 
 
@@ -82,6 +98,36 @@ class MainActivity : AppCompatActivity() {
         gameStarted = false
     }
 
+    // Restore values after configuration change
+    private fun restoreGame() {
+
+        // Your Score
+        val restoredScore = getString(R.string.your_score, score)
+        gameScoreTextView.text = restoredScore
+
+        // Time Left
+        val restoredTime = getString(R.string.time_left, timeLeft)
+        timeLeftTextView.text = restoredTime
+
+        countDownTimer = object : CountDownTimer((timeLeft * 1000).toLong(), countDownInterval) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left, timeLeft)
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+        }
+
+        countDownTimer.start()
+        gameStarted = true
+
+    }
+
     // start game logic
     private fun startGame() {
         countDownTimer.start()
@@ -94,4 +140,29 @@ class MainActivity : AppCompatActivity() {
         resetGame()
     }
 
+    /*****
+     * Preparing the app for saving data from being lost due to "Configuration Change"
+     */
+    companion object {
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
+
+    // Prepare important DATA before the app will be destroyed due to configuration change
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeLeft)
+
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score & Time Left: $timeLeft")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy Called")
+    }
 }
