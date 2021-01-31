f78444204858126b33008e0fe9c5303f48576632
abubaker <mohammad_abubaker@yahoo.com>
2021-01-31 18:47:31 +0500

    Implemented basic logic for incrementScore()

--- app/src/main/java/org/abubaker/timefighter/MainActivity.kt
+++ app/src/main/java/org/abubaker/timefighter/MainActivity.kt
@@ -1,13 +1,59 @@
 package org.abubaker.timefighter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // We will connect views to these variables later on
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapButton: Button
+
+    // DATA
+    private var score = 0
+
+
+    // onCreate - Connect views to the variables
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Comment
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapButton = findViewById(R.id.tap_me_button)
+
+        tapButton.setOnClickListener {
+            incrementScore()
+        }
     }
+
+
+    // Increment score logic
+    private fun incrementScore() {
+
+        score++
+
+        val newScore = "Your Score: $score"
+        gameScoreTextView.text = newScore
+
+    }
+
+    // Reset game logic
+    private fun resetGame() {
+
+    }
+
+    // start game logic
+    private fun startGame() {
+
+    }
+
+    // end game logic
+    private fun endGame() {
+
+    }
+
 }
