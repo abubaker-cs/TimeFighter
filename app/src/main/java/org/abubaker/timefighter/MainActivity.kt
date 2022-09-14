f9e1feb9f83cedf1bea50e2140e9f8904ed655f3
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-09-14 22:42:11 +0500

    Created incrementScore() function.

--- app/src/main/java/org/abubaker/timefighter/MainActivity.kt
+++ app/src/main/java/org/abubaker/timefighter/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var score = 0
+
     // onCreate()
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -16,6 +18,19 @@ class MainActivity : AppCompatActivity() {
 
         // Inflate Layout: @layout/activity_main.xml
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.tapMeButton.setOnClickListener { _ ->
+            incrementScore()
+        }
+
+        // It will display the initial (default) value of the score = 0
+        binding.gameScoreTextView.text = getString(R.string.yourScore, score)
+    }
+
+    private fun incrementScore() {
+        score += 1
+        val newScore = getString(R.string.yourScore, score)
+        binding.gameScoreTextView.text = newScore
     }
 
 }
